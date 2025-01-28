1985f78a6e61cbf670d1dff62b19cacb187aafcd
A-Farabi <farabisyl2017@gmail.com>
2025-01-28 19:04:16 +0600

    show remaining without reload

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,21 @@
 import { useLoaderData } from 'react-router-dom'
 import './App.css'
 import CoffeeCard from './Components/CoffeeCard'
+import { useState } from 'react'
 
 
 function App() {
 
-  const coffees = useLoaderData()
+  const loadedCoffees = useLoaderData()
+
+  const [coffees, setCoffees] = useState(loadedCoffees)
+
 
   return (
     <>
     <h1>Hot coffees: {coffees.length}</h1>
     {
-      coffees.map(coffee => <CoffeeCard key={coffee._id} coffee={coffee}></CoffeeCard>)
+      coffees.map(coffee => <CoffeeCard key={coffee._id} coffee={coffee} coffees={coffees} setCoffees={setCoffees}></CoffeeCard>)
     }
     </>
   )
